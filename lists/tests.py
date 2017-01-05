d38a1ebc7a37cbe8336810f6a451e3e84f97085d
dani_pni <daniela.ponader@gmail.com>
2017-01-05 08:25:15 -0500

    first unit test, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,10 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_homepage_view(self):
+        #Django uses internally to resolve URLs resolve
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
